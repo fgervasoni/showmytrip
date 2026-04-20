d1e4ab8552b2a126a18341e99e61b7901a55120a
fgervasoni <federico_gervasoni@yahoo.com>
2026-04-20 14:53:30 +0200

    Remove Tailwind integration from Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
-import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind()],
+	integrations: [],
 	output: 'server',
 	adapter: node({
 		mode: 'standalone',
